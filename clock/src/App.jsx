e0f89504ddcc753cbf4d76314b3f2a825a5ed30d
Sahitya90 <sahityavikramsingh@gmail.com>
2025-11-25 19:43:57 +0530

    Intial Commit

---
+++ clock/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Clock from './Clock.jsx'
+
+function App() {
+  
+  return (
+   <Clock/>
+  )
+}
+
+export default App
---
+++ clock/src/Clock.jsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+
+function Clock(){
+
+    const [time, setTime]= useState(new Date());
+
+    useEffect(()=>{
+        const IntervalId = setInterval(()=> {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(IntervalId);
+        }
+    }, []);
+
+    function formatTime(){
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        const meridium = hours >= 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)}: ${padZero(minutes)}: ${padZero(seconds)}  ${meridium}`;
+    }
+
+    function padZero(number){
+        return (number < 10 ? "0":"") + number;
+    }
+
+    return(
+    <div className='clock-container'>
+        <div className='clock'>
+            <span>{formatTime()}</span>
+        </div>
+    </div>
+    )
+}
+export default Clock
